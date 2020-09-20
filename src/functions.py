0c3698939c204f9733592e260e52f9ae8fe99858
Erfan Nariman <erfannariman@gmail.com>
2020-09-20 22:07:58 +0200

    first version

--- src/functions.py
+++ src/functions.py
@@ -6,15 +6,3 @@ import pandas as pd
 def check_files_downloaded():
     if len(os.listdir()) == 0:
         GetFiles().download_csvs()
-
-
-files = os.listdir("../data")
-file = files[0]
-df = pd.read_csv(f"../data/{file}")
-print(df)
-
-
-
-
-
-
